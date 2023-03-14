6fffeabd99233f78627f41aa96b934d9a7059a7c
hanxu <hanxuit@163.com>
2023-03-14 18:03:41 +0800

    update Summary.vue

--- src/components/Summary.vue
+++ src/components/Summary.vue
@@ -164,17 +164,159 @@
         <span class="bold">舒适环境</span>
       </span>
       <div class="img-preview">
-        <div class="preview-img environment-one"></div>
-        <div class="preview-img environment-two"></div>
-        <div class="preview-img environment-three"></div>
+        <div class="preview-img environment-one">
+          <div class="mask">
+            <div class="container">
+              <div class="title">新生儿室</div>
+              <div class="underline"></div>
+              <div class="content">月子中心贵宾室，为妈妈们提供了舒适的 休闲沟通场所，所有家具均进口自韩国，
+                材质环保，奢华大气。
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="preview-img environment-two">
+          <div class="mask">
+            <div class="container">
+              <div class="title">新生儿室</div>
+              <div class="underline"></div>
+              <div class="content">月子中心贵宾室，为妈妈们提供了舒适的 休闲沟通场所，所有家具均进口自韩国，
+                材质环保，奢华大气。
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="preview-img environment-three">
+          <div class="mask">
+            <div class="container">
+              <div class="title">新生儿室</div>
+              <div class="underline"></div>
+              <div class="content">月子中心贵宾室，为妈妈们提供了舒适的 休闲沟通场所，所有家具均进口自韩国，
+                材质环保，奢华大气。
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div></div>
+      <div class="item-preview">
+        <div class="item">
+          新生儿室 <br>
+          NEONATAL ROOM
+        </div>
+        <div class="item">
+          美容美体室 <br>
+          BEAUTY ROOM
+        </div>
+        <div class="item">
+          周边环境 <br>
+          SURROUNDINGS
+        </div>
+        <div class="item">
+          阳光瑜伽房 <br>
+          YOGA ROOM
+        </div>
+        <div class="item">
+          御膳房 <br>
+          KITCHEN
+        </div>
+        <div class="item">
+          感染防护 <br>
+          PROTECTION
+        </div>
+        <div class="item">
+          安保隐私 <br>
+          SECURITY
+        </div>
+        <div class="item">
+          贵宾室 <br>
+          VIP ROOM
+        </div>
+      </div>
+
+    </div>
+    <div class="professional-team">
+      <div class="title">
+        体贴入微的专业团队
+      </div>
+      <div class="content">
+        <div class="left">
+          <div class="top"></div>
+          <div class="middle"></div>
+          <div class="bottom"></div>
+        </div>
+        <div class="right">
+          <div class="top"></div>
+          <div class="bottom">
+            <div class="container">
+              <div class="avatar"></div>
+              <div class="desc">
+                <div class="desc-title">齐国华教授</div>
+                <div class="desc-content">齐国华教授10多年从医经验, 在妇产科领域的研究与临床诊疗方面有丰
+                  富的临床经验。在各种疑难杂症及危重病人抢救...
+                </div>
+              </div>
+            </div>
+            <el-button color="#FFBF1C" class="right-button" type="warning" :icon="Plus"></el-button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="love-story">
+      <div class="title">
+        新生的喜悦，<b>一切关于爱的故事</b>
+      </div>
+      <div class="story-content">
+          <div class="content-left">
+            <div class="comment">
+              <img src="../assets/summary/avatar2.png" alt="">
+              <div>
+                <div class="comment-title">
+                  月子中心-深圳辣妈 李女士
+                </div>
+                <div class="comment-content">
+                  虽然在孕期已经对宝宝的到来做了很多准备，但毕竟是第 一次当妈妈，有时候感觉自己...
+                </div>
+              </div>
+            </div>
+            <div class="comment">
+              <img src="../assets/summary/avatar3.png" alt="">
+              <div>
+                <div class="comment-title">
+                  月子中心-深圳辣妈 李女士
+                </div>
+                <div class="comment-content">
+                  虽然在孕期已经对宝宝的到来做了很多准备，但毕竟是第 一次当妈妈，有时候感觉自己...
+                </div>
+              </div>
+            </div>
+            <div class="comment">
+              <img src="../assets/summary/avatar4.png" alt="">
+              <div>
+                <div class="comment-title">
+                  月子中心-深圳辣妈 李女士
+                </div>
+                <div class="comment-content">
+                  虽然在孕期已经对宝宝的到来做了很多准备，但毕竟是第 一次当妈妈，有时候感觉自己...
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="content-right">
+            <img class="love-video" src="../assets/summary/love-video.png" alt="">
+            <img class="play" src="../assets/summary/play-button.png" alt="播放">
+          </div>
+      </div>
+    </div>
+    <div class="service">
+      <div class="service-title">
+        专业服务 , <b>只为享受月子的美好</b>
+      </div>
     </div>
   </div>
 </template>
 
-<script>
-
+<script setup>
+import {Plus} from "@element-plus/icons-vue";
 </script>
 
 <style lang="scss" scoped>
@@ -184,6 +326,7 @@
   background: url("@/assets/banner.png") no-repeat center center;
   background-size: 100% 100%;
 }
+
 .banner-description {
   width: 100%;
   height: 251px;
@@ -194,6 +337,7 @@
   padding: 0 10%;
   justify-content: space-around;
   align-content: space-around;
+
   .desc-item {
     width: max-content;
     text-align: center;
@@ -202,16 +346,20 @@
     gap: 2em;
     justify-content: center;
   }
+
   .desc-title {
     font-size: 30px;
     color: rgba(51, 51, 51, 1);
   }
+
   .desc-content {
     font-size: 18px;
     color: rgba(102, 102, 102, 1);
   }
 }
+
 .introduce {
+  padding: 0 3em;
   color: #333333;
   text-align: center;
   height: 950px;
@@ -270,14 +418,17 @@
     flex-direction: column;
     justify-content: flex-start;
     width: 50%;
+
     .introduce-content-right-title {
       text-align: left;
+
       .left-title {
         height: 42px;
         font-size: 30px;
         color: #333333;
         line-height: 35px;
       }
+
       .right-title {
         height: 41px;
         font-size: 30px;
@@ -287,10 +438,12 @@
         line-height: 35px;
       }
     }
+
     .introduce-content-right-content {
       .topic {
         display: flex;
         justify-content: flex-start;
+
         img {
           margin-top: 8px;
           line-height: 30px;
@@ -305,7 +458,9 @@
             opacity: 1;
             border: 1px solid #D2D2D2;
           }
+
           margin-left: 1em;
+
           h3 {
             height: 48px;
             font-size: 24px;
@@ -315,6 +470,7 @@
             line-height: 48px;
             margin-bottom: 0.6em;
           }
+
           .symbol {
             margin: 0 1em 0 0;
             display: inline-block;
@@ -330,6 +486,7 @@
             justify-content: flex-start;
             flex-wrap: wrap;
             gap: 1em 3em;
+
             .text {
               height: 25px;
               font-size: 18px;
@@ -343,38 +500,315 @@
     }
   }
 }
+
 .comfortable {
   text-align: center;
   width: 100%;
-  height: 1000px;
+  padding: 2em 0;
   background-color: #f2f2f2;
+
   .title {
     display: inline-block;
     margin: 0.6em 0 0.4em 0;
     font-size: 46px;
+
     .bold {
       font-weight: bold;
     }
   }
+
   .img-preview {
+    padding: 0 2em;
     display: flex;
     justify-content: space-around;
+    cursor: pointer;
+
     .preview-img {
       width: 464px;
       height: 564px;
+
+      .mask {
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        opacity: 0.7;
+        background-color: var(--el-color-primary); /* Adjust the opacity to your liking */
+        z-index: 1;
+        display: none;
+        color: #FFFFFF;
+
+        .container {
+          height: 100%;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+          vertical-align: center;
+          padding: 0 20%;
+          gap: 0.6em;
+
+          .title {
+            margin-bottom: 0;
+            font-size: 24px;
+            font-weight: 500;
+            line-height: 28px;
+          }
+
+          .underline {
+            width: 2em;
+            height: 3px;
+            background: rgba(255, 255, 255, 0.85);
+            border-radius: 0;
+            opacity: 1;
+          }
+        }
+      }
     }
+
     .environment-one {
       background: url("@/assets/summary/environment-one.png") no-repeat center center;
       background-size: 100% 100%;
     }
+
+    .environment-one:hover {
+      .mask {
+        display: block;
+      }
+    }
+
     .environment-two {
       background: url("@/assets/summary/environment-two.png") no-repeat center center;
       background-size: 100% 100%;
     }
+
+    .environment-two:hover {
+      .mask {
+        display: block;
+      }
+    }
+
     .environment-three {
       background: url("@/assets/summary/environment-three.png") no-repeat center center;
       background-size: 100% 100%;
     }
+
+    .environment-three:hover {
+      .mask {
+        display: block;
+      }
+    }
+  }
+
+  .item-preview {
+    cursor: pointer;
+    padding: 3em 4em 0 4em;
+    display: flex;
+    justify-content: space-around;
+
+    .item {
+      padding: 2em 2em;
+      border-style: solid;
+      border-color: rgba(210, 210, 210, 1);
+      border-width: 2px 0 2px 0;
+    }
+
+    .item:hover {
+      border-color: var(--el-color-primary);
+      color: var(--el-color-primary);
+    }
+  }
+}
+
+.professional-team {
+  text-align: center;
+
+  .title {
+    margin: 0.8em 0 0.6em 0;
+    height: 64px;
+    font-size: 46px;
+    font-weight: bold;
+    color: #333333;
+  }
+
+  .content {
+    padding: 1em 2em;
+    display: flex;
+    justify-content: space-around;
+    gap: 2em;
+
+    .left {
+      width: 30%;
+      display: flex;
+      flex-direction: column;
+      gap: 1.4em;
+
+      .top {
+        height: 250px;
+        background: url("@/assets/summary/team-top.png") no-repeat center center;
+        background-size: 100% 100%;
+      }
+
+      .middle {
+        height: 250px;
+        background: url("@/assets/summary/team-middle.png") no-repeat center center;
+        background-size: 100% 100%;
+      }
+
+      .bottom {
+        height: 250px;
+        background: url("@/assets/summary/team-bottom.png") no-repeat center center;
+        background-size: 100% 100%;
+      }
+    }
+
+    .right {
+      width: 60%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      .top {
+        width: 70%;
+        height: 470px;
+        background: url("@/assets/summary/team-right-top.png") no-repeat center center;
+        background-size: 100% 100%;
+        z-index: 10;
+      }
+
+      .bottom {
+        width: 100%;
+        height: 40%;
+        background-color: var(--el-color-primary);
+        display: flex;
+        justify-content: center;
+        gap: 0.1em;
+
+        .container {
+          width: 80%;
+          position: relative;
+          background-color: #FFFFFF;
+          margin: 2.6em 0;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          padding: 0 2em;
+          text-align: left;
+
+          .avatar {
+            min-height: 100px;
+            min-width: 100px;
+            background: url("@/assets/summary/avatar.png") no-repeat center center;
+            background-size: contain;
+            margin-right: 1em;
+          }
+
+          .desc {
+            .desc-title {
+              font-size: 20px;
+              font-weight: 500;
+              color: #333333;
+            }
+
+            .desc-content {
+              font-size: 15px;
+              font-weight: 400;
+              color: #666666;
+              line-height: 22px;
+            }
+          }
+        }
+
+        .right-button {
+          height: auto;
+          margin: 2.8em 0;
+          padding: 0 1.6em;
+          left: 4px;
+        }
+      }
+    }
+  }
+}
+
+.love-story {
+  text-align: center;
+  width: 100%;
+  padding: 2em 0;
+  background-color: #F8F8F8;
+
+  .title {
+    font-size: 46px;
+    font-weight: bold;
+    color: #333333;
+  }
+
+  .story-content {
+    padding: 2em 2em 4em 2em;
+    display: flex;
+    justify-content: space-around;
+    .content-left {
+      width: 40%;
+      .comment {
+        margin-bottom: 1.6em;
+        display: flex;
+        align-items: center;
+        gap: 1em;
+        img {
+          width: 100px;
+          height: 100px;
+        }
+        .comment-title {
+          text-align: left;
+          margin-bottom: 0.4em;
+          font-size: 20px;
+          color: #666666;
+        }
+
+        .comment-content {
+          background-color: #FFFFFF;
+          padding: 1em 2em;
+          font-size: 16px;
+          font-weight: 400;
+          color: #666666;
+        }
+      }
+    }
+
+    .content-right {
+      background-color: rgba(210, 210, 210, 0.1);
+      width: 801px;
+      height: 446px;
+      .love-video {
+        position: relative;
+        opacity: 1;
+        width: 100%;
+        height: 100%;
+        right: 3em;
+        top: 3em;
+      }
+      .play {
+        cursor: pointer;
+        position: absolute;
+        height: 84px;
+        width: 84px;
+        top: 50%;
+        left: 40%;
+
+      }
+    }
+  }
+}
+
+.service {
+  text-align: center;
+  width: 100%;
+  padding: 2em 0;
+  .service-title {
+    font-size: 46px;
+    font-weight: bold;
+    color: #333333;
+    line-height: 54px;
+    -webkit-background-clip: text;
   }
 }
 
